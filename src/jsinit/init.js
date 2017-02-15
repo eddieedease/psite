516d39d09ad9bf1e17a85a455e4c5ba0438d86ef
Eddie Maas <eddie@edease.nl>
2017-02-15 15:16:40 +0100

    little flair

---
+++ src/jsinit/init.js
@@ -0,0 +1,8 @@
+(function($){
+  $(function(){
+
+    // Activating for the main component
+    $('.button-collapse').sideNav();
+	$('.scrollspy').scrollSpy();
+	}); // end of document ready
+})(jQuery); // end of jQuery name space
