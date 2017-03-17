30b0b6305f62a1a2fb1233a2d29e2b3b0e762ad8
Eddie Maas <eddie@edease.nl>
2017-03-17 08:22:26 +0100

    logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule, JsonpModule } from '@angular/http';
+import {Ng2PaginationModule} from 'ng2-pagination'; 
 
 import { AppComponent } from './app.component';
 import { SiteComponent } from './site/site.component';
@@ -15,7 +16,8 @@ import { SiteComponent } from './site/site.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    JsonpModule 
+    JsonpModule,
+    Ng2PaginationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
