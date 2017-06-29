3f618bf8c4b5a812fa7e25d55512f5fe18525660
eddieedease <eddie@edease.nl>
2017-06-29 22:58:01 +0200

    cubestick update

--- src/app/terminal/terminal.component.ts
+++ src/app/terminal/terminal.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
 
 @Component({
   selector: 'app-terminal',
@@ -7,131 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TerminalComponent implements OnInit {
 
-    commands = [{
-    name: "clear",
-    function: clearConsole
-}];
+  text: any;
+  selff ;
 
- cursor = $('#cursor');
- terminal = $('#terminal');
- text = ["GLaDOS v1.09 (c) 1982 Aperture Science, Inc.\nWARN: This site is under construction\n$> ", ""];
- commandHistrory = [];
- lineY = 1;
- index = 0;
- historyIndex = 0;
+  timer;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-    window.setInterval(function () {
-    this.cursor.toggleClass('invisible');
-  }, 500);
+    this.text = $('.test').data('text');
+    this.selff = this;
+    
+    //this.typeWriter(this.text, 0);
+    this.typeWriter(this.text,0);
+  }
+
+ 
+
+  typeWriter(_text, n) {
+    if (n < (_text.length)) {
+      $('.test').html(_text.substring(0, n + 1));
+      n++;
+
+        this.timer = setTimeout(() => this.selff.typeWriter(_text, n), 50);
+        
+      
+    }
   }
 
 
 
- 
 
-  
 
- clearConsole() {
-    this.text = [];
-    this.lineY = 0;
-}
 
- printConsole(txt) {
-    this.cursor.remove();
-    this.terminal.html(text);
-    this.terminal.append('<span id="cursor"></span>');
-    this.cursor = $('#cursor');
-}
 
-processCommand(rawData) {
-    let args = rawData.split(" ");
-    let command = args[0];
-    this.commandHistrory[historyIndex] += rawData;
-    this.args.shift();
-    let unknownComand = true;
-    for (let i = 0; i < this.commands.length; i++) {
-        if (command === this.commands[i].name) {
-            this.commands[i].function(args);
-            unknownComand = false;
-            break;
-        }
-    }
-    if (unknownComand == true) {
-        this.lineY++;
-        this.text[lineY] = "\nsystem: command not found";
-    }
-    this.historyIndex++;
-}
 
-nextLine() {
-    processCommand(this.text[this.lineY]);
-    if (this.lineY != 0) {
-        this.lineY++;
-        this.text[this.lineY] = "\n";
-    }
-    else
-        this.text[this.lineY] = "";
-
-    this.text[this.lineY] += "$> ";
-    this.lineY++;
-    this.text[this.lineY] = "";
-    printConsole(this.text);
-}
-
-erase(n) {
-    this.text[this.lineY] = this.text[this.lineY].slice(0, -n);
-    this.index--;
-    printConsole(this.text);
-}
-
-// tslint:disable-next-line:one-line
-$(document).ready(function () {
-    printConsole(this.text);
-});
-
-// tslint:disable-next-line:member-ordering
-$('html'; ).on('keydown', function (e) {
-    e = e || window.event;
-    let keyCode = typeof e.which === "number" ? e.which : e.keyCode;
-
-    // Backspace? Erase!
-    if (keyCode === 8 && e.target.tagName !== "INPUT" && e.target.tagName !== "TEXTAREA") {
-        e.preventDefault();
-        if (index != 0)
-            erase(1);
-    }
-});
-
-$(document).keypress(function (e) {
-    // Make sure we get the right event
-    e = e || window.event;
-    var keyCode = typeof e.which === "number" ? e.which : e.keyCode;
-
-    // Which key was pressed?
-    switch (keyCode) {
-        // ENTER
-        case 13:
-        {
-            nextLine();
-            break;
-        }
-        default:
-        {
-            var data = String.fromCharCode(keyCode);
-            if (data != undefined) {
-                var length = text[lineY].length;
-                text[lineY] += data;
-                index = index + (text[lineY].length - length);
-                printConsole(text);
-            }
-          break;
-        }
-    }
-});
-  
 
 
 
