784dc9f024cad9b449dba856ee52b0f1b629849b
eddieedease <eddie@edease.nl>
2017-07-03 16:48:53 +0200

    updated packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule, JsonpModule } from '@angular/http';
 import {Ng2PaginationModule} from 'ng2-pagination'; 
+import {Angular2ImageGalleryModule} from 'angular2-image-gallery';
 
 import { AppComponent } from './app.component';
 import { SiteComponent } from './site/site.component';
@@ -19,7 +20,8 @@ import { TerminalComponent } from './terminal/terminal.component';
     FormsModule,
     HttpModule,
     JsonpModule,
-    Ng2PaginationModule
+    Ng2PaginationModule,
+    Angular2ImageGalleryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
